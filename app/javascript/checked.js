bb532356d3f1c7c8f4b955d2a51c27e11eceec0f
purinmai <maaai.0307@gmail.com>
2021-01-12 20:00:49 +0900

    提供済みメニューのスタイル

--- app/javascript/checked.js
+++ app/javascript/checked.js
@@ -1,12 +1,15 @@
 const check = ()=> {
   document.querySelector('button').addEventListener('click', () => {
-    const boxes = document.querySelectorAll('input');
-  
-      boxes.forEach(box => {
-        if (box.checked === true) {
-          box.disabled = true;
-          };
-      });
+    // どのインプットのチェックがついたか？
+    const inputs = document.querySelectorAll('input');
+    inputs.forEach (function(input){
+        if (input.checked === true) {
+          input.disabled = true;
+          const row = input.parentNode.parentNode;
+          row.classList.add("checked");  
+        };
+    });
   });
+   
 };
 window.addEventListener("load", check);
