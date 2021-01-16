b2101af3cc082555c4ec93812e5f6707d6ea2512
purinmai <maaai.0307@gmail.com>
2021-01-16 18:02:23 +0900

    check_box を使用不可に

--- app/javascript/checked.js
+++ app/javascript/checked.js
@@ -1,15 +1,15 @@
-const check = ()=> {
-  document.querySelector('button').addEventListener('click', () => {
-    // どのインプットのチェックがついたか？
-    const inputs = document.querySelectorAll('input');
-    inputs.forEach (function(input){
-        if (input.checked === true) {
-          input.disabled = true;
-          const row = input.parentNode.parentNode;
-          row.classList.add("checked");  
-        };
-    });
-  });
+// const check = ()=> {
+//   document.getElementById('order-btn-space').addEventListener('click', () => {
+//     // どのインプットのチェックがついたか？
+//     const inputs = document.querySelectorAll('input');
+//     inputs.forEach (function(input){
+//         if (input.checked === true) {
+//           input.disabled = true;
+//           const row = input.parentNode.parentNode;
+//           row.classList.add("checked");  
+//         };
+//     });
+//   });
    
-};
-window.addEventListener("load", check);
+// };
+// window.addEventListener("load", check);
